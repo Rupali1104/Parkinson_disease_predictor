ed1fd4436fb19f74fd04446c3168d407df3c9777
Abhishek Pai <167780676+paiabhishekpai@users.noreply.github.com>
2024-11-07 20:37:19 +0530

    checkpoint files

---
+++ app-checkpoint.py
@@ -0,0 +1,28 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_text = request.form['text']
+    input_text_sp = input_text.split(',')
+    np_data = np.asarray(input_text_sp, dtype=np.float32)
+    prediction = model.predict(np_data.reshape(1, -1))
+
+    if prediction == 1:
+        output = "This person has Parkinson's disease"
+    else:
+        output = "This person does not have Parkinson's disease"
+
+    return render_template("index.html", message=output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
